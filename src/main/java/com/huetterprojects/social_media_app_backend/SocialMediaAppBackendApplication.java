2b9e81f29aca6e11e6e60c854f8858c18b26d995
Reinhard Huetter <reinhard.huetter.privat@gmail.com>
2026-03-05 09:24:19 +0100

    first commit project created

---
+++ src/main/java/com/huetterprojects/social_media_app_backend/SocialMediaAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.huetterprojects.social_media_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaAppBackendApplication.class, args);
+	}
+
+}
